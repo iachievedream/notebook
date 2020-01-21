7e9fd9e6626af1bdaa8322cc9da4c3f14b1b66ad
iachievedream <iachievedream@gmail.com>
2020-01-21 13:38:21 +0800

    update content

--- linux/test_sh/appach.sh
+++ linux/test_sh/appach.sh
@@ -3,6 +3,7 @@ sudo apt install vim
 # install yum，npm，nodejs，curl
 sudo apt-get update 
 sudo apt install yum
+
 sudo apt-get install npm
 sudo apt-get install nodejs
 sudo apt-get install curl
@@ -24,6 +25,13 @@ sudo apt-get install mysql-server
 sudo apt install mysql-client
 sudo apt install libmysqlclient-dev
 
+# 安裝 java 8 & maven
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
 
-
-
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+# http://127.0.0.1:8000
